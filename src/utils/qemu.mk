a039bad36e3bc2554defe95f44ff869f2d6aecfa
Steven Kang <kangweibaby@163.com>
2023-10-30 12:15:47 +0800

    🌱 master boot sector

---
+++ src/utils/qemu.mk
@@ -0,0 +1,16 @@
+
+QEMU+= qemu-system-i386 # qemu virtual machine
+QEMU+= -m 32M # memory
+QEMU+= -drive file=$(BUILD)/harddisk.img,if=ide,index=0,media=disk,format=raw # harddisk
+
+QEMU_DISK_BOOT:=-boot c
+
+QEMU_DEBUG:= -s -S # qemu debug
+
+.PHONY: qemu
+qemu: $(IMAGES)
+	$(QEMU) $(QEMU_DISK_BOOT)
+
+.PHONY: qemug
+qemug: $(IMAGES)
+	$(QEMU) $(QEMU_DISK_BOOT) $(QEMU_DEBUG)
