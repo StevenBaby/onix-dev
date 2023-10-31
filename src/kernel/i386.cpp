46358f7568432742f7529e895ecf4ad417077936
Steven Kang <kangweibaby@163.com>
2023-10-31 11:09:40 +0800

    ✨ i386 init

---
+++ src/kernel/i386.cpp
@@ -0,0 +1,15 @@
+#include <onix/types.h>
+
+static const char message[] = "Onix running in protected mode...";
+
+namespace i386
+{
+    _extern void i386_init()
+    {
+        char *video = (char *)0xb8000;
+        for (int i = 0; i < sizeof(message); i++)
+        {
+            video[i * 2] = message[i];
+        }
+    }
+}
