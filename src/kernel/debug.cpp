71cc59b3c6ef794e4f774c3cea90810fd6214176
Steven Kang <kangweibaby@163.com>
2023-10-31 22:09:02 +0800

    ✨ debug

---
+++ src/kernel/debug.cpp
@@ -0,0 +1,24 @@
+#include <onix/debug.h>
+#include <onix/device.h>
+#include <onix/debug.h>
+#include <onix/stdarg.h>
+#include <onix/printk.h>
+
+namespace arch
+{
+    static char buf[1024];
+
+    void debugk(char *file, int line, const char *fmt, ...)
+    {
+        device::device_t *ptr = device::instance(device::DEVICE_CONSOLE);
+
+        int i = sprintf(buf, "[%s] [%d] ", file, line);
+        ptr->write(buf, i);
+
+        va_list args;
+        va_start(args, fmt);
+        i = vsprintf(buf, fmt, args);
+        va_end(args);
+        ptr->write(buf, i);
+    }
+}
--- src/kernel/i386.cpp
+++ src/kernel/i386.cpp
@@ -2,11 +2,14 @@
 #include <onix/io.h>
 #include <onix/device.h>
 #include <onix/printk.h>
+#include <onix/debug.h>
+
+#define LOGK(fmt, args...) DEBUGK(fmt, ##args)
 
 _extern void i386_init()
 {
     device::device_init();
     device::none_init();
     device::console_init();
-    printk("onix running in protected mode...\n");
+    LOGK("onix running in protected mode...\n");
 }
