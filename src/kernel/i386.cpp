f7deb14b47864235839ca7d1be69006ebe91f9a8
Steven Kang <kangweibaby@163.com>
2023-10-31 22:07:01 +0800

    ✨ printk

--- src/kernel/i386.cpp
+++ src/kernel/i386.cpp
@@ -1,10 +1,12 @@
 #include <onix/types.h>
 #include <onix/io.h>
 #include <onix/device.h>
+#include <onix/printk.h>
 
 _extern void i386_init()
 {
     device::device_init();
     device::none_init();
     device::console_init();
+    printk("onix running in protected mode...\n");
 }
